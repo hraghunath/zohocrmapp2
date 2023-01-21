2525efaae74a6c124ca270edb7f34225844f9e49
hraghunath <hraghunath08@gmail.com>
2023-01-21 21:22:09 +0530

    zohocrmapp2 backup

---
+++ src/main/java/com/zoho/util/EmailServiceImpl.java
@@ -0,0 +1,22 @@
+package com.zoho.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+@Component
+public class EmailServiceImpl implements EmailService {
+    @Autowired  
+	private JavaMailSender sendEmail;
+	@Override
+	public void sendEmail(String to, String sub, String email) {
+		SimpleMailMessage mail=new SimpleMailMessage();
+		mail.setTo(to);
+		mail.setSubject(sub);
+		mail.setText(email);
+		 
+		sendEmail.send(mail);
+
+	}
+
+}
